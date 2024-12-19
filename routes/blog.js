4e4c2824bcc5b93eb3c75dfa84b18b026a90e51c
Shuvam-2000 <shuvamsaha2000@gmail.com>
2024-12-19 11:54:39 +0530

    update: added file upload functioanlity for blog uploading with multer and add it to the database

--- routes/blog.js
+++ routes/blog.js
@@ -1,7 +1,21 @@
 const { Router } = require('express');
 const Blog = require('../models/blog');
+const multer = require('multer');
+const path = require('path');
 
 const router = Router();
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.resolve(`./public/uploads/`));
+    },
+    filename: function (req, file, cb) {
+      const fileName = `${Date.now()}-${file.originalname}`;
+      cb(null, fileName);
+    },
+  })
+
+const upload = multer({ storage: storage })
 
 // routes for adding new blog
 router.get('/add-new', (req,res) => {
@@ -11,10 +25,15 @@ router.get('/add-new', (req,res) => {
 })
 
 // route for adding blogs
-router.post('/', (req,res) => {
-    return res.render('addblog', {
-        user: req.user,
+router.post('/', upload.single('coverImage'), async (req,res) => {
+    const { title, body } = req.body;
+    const blog = await Blog.create({
+      body,
+      title,
+      createdBy: req.user._id,
+      coverImageURL: `/uploads/${req.file.filename}`
     })
-})
+    return res.redirect(`/blog/${blog._id}`);
+    });
 
 module.exports = router;
